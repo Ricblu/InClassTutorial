20b9a462d5b0f69b5aad431d81374fa002a4a302
unknown <Richard.Dorta@iam.local>
2018-11-21 15:13:53 -0600

    Finished Episode 11
    I update the check points to be able to grow, rotate, and change opacity all depending on what checkpoint is currently active or not.

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,6 +4,65 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField]
+    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
+
+    [SerializeField]
+    private float inacvtivatedScale = 1, activatedScale = 1.5f;
+
+    [SerializeField]
+    private Color inactivatedColor, activatedColor;
+
+    private bool isActived = false;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactivatedColor;
+
+        if (isActived)
+            color = activatedColor;
+
+        spriteRenderer.color = color;
+    }
+
+    private void UpdateScale()
+    {
+        float scale = inacvtivatedScale;
+
+        if (isActived)
+            scale = activatedScale;
+
+        transform.localScale = Vector3.one * scale;
+    }
+
+    private void UpdateRotation()
+    {
+        float rotationSpeed = inactivatedRotationSpeed;
+
+        if (isActived)
+            rotationSpeed = activatedRotationSpeed;
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActived = value;
+        UpdateScale();
+        UpdateColor();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
